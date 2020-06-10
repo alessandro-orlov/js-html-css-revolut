ec3f119b68b763edd0cdc0c8f331e09f401c2d45
Alessandro  (Notebook) <alex.ixs@hotmail.com>
2020-06-10 16:42:06 +0200

    Aggiunto il menu delle lingue che si apre e si chiude al click
    In piu' il menu delle lingue si resetta quando il mouse apre un altro menu a tendina(dropdown)

--- js/myscript.js
+++ js/myscript.js
@@ -2,16 +2,38 @@ $(document).ready(
   function() {
 
     // Apro il dropdown quando il mouse si trova sul li
-    // che ha classe.with-dropdown
+    // che ha classe .with-dropdown
     $('.with-dropdown').mouseenter(
       function() {
         $(this).children('.dropdown').addClass('open');
+        $('.lang-select .dropdown').removeClass('open');
+        $('.lang-select i').removeClass('fa-angle-up');
+        $('.lang-select i').addClass('fa-angle-down');
       }
     );
 
+    // Chudo il menu quando il mouse esce dal area del li con la classe
+    // .with-dropdown
     $('.with-dropdown').mouseleave(
       function() {
         $(this).children('.dropdown').removeClass('open');
+      }
+    );
+
+    // Apro il menu della selezione delle libgue al click
+    $('.lang-select').click(
+      function() {
+        // Campio la freccettina up/down in base all'apertura del menu
+        if($('i').hasClass('fa-angle-down')) {
+          $('i').removeClass('fa-angle-down');
+          $('i').addClass('fa-angle-up');
+        } else {
+            $('i').removeClass('fa-angle-up');
+            $('i').addClass('fa-angle-down');
+          }
+
+        // Apro o chiudo il menu delle lingue
+        $(this).children('.dropdown').toggleClass('open');
       }
     );
 
